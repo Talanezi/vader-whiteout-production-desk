110493809219eca6059c267b1cd9f3896d07d9e6
Talanezi <talanezi@ucsd.edu>
2026-04-20 19:14:09 +0000

    Remove placeholder callsheets route from app controller

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -9,9 +9,4 @@ export class AppController {
   getHealth() {
     return this.appService.getHealth();
   }
-
-  @Get('callsheets')
-  getCallSheets() {
-    return this.appService.getCallSheets();
-  }
 }
