f73d91ad6535aba1a5a9a1bf651dd160fa24cf46
Talanezi <talanezi@ucsd.edu>
2026-04-20 22:06:06 +0000

    Import AuthModule into CallsheetsModule for scheduler auth guard

--- server/src/callsheets/callsheets.module.ts
+++ server/src/callsheets/callsheets.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 import { CallsheetsController } from './callsheets.controller';
 import { CallsheetsService } from './callsheets.service';
 import { CallSheetDraftEntity } from './entities/callsheet-draft.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CallSheetDraftEntity])],
+  imports: [
+    TypeOrmModule.forFeature([CallSheetDraftEntity]),
+    AuthModule,
+  ],
   controllers: [CallsheetsController],
   providers: [CallsheetsService],
   exports: [CallsheetsService],
